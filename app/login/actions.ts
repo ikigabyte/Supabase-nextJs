b3ca2e26cd0e9988124094fe4ed5e36a089b7a70
Kigabyte <kevr1999@gmail.com>
2025-04-24 11:05:18 -0400

    Implement authentication actions for login and signup with Supabase

---
+++ app/login/actions.ts
@@ -0,0 +1,43 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+// Email and password for formData
+export async function emailLogin(formData: FormData) { 
+  const supabase = await createClient()
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+
+  // todo Form validation methods
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  const { error } = await supabase.auth.signInWithPassword(data)
+  if (error) {
+    console.error('Error logging in', error)
+    redirect('/login?message=Could not authenticate user')
+  }
+  revalidatePath('/', 'layout') // * Clearing the cache for new cache entries with the user logging in
+  console.log("VALID USER LOGGING IN NOW")
+  redirect('/todos') // * Redirecting to the todos page
+  // console.log("going in now")
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient() // *Very important from the supabase server file
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  const { error } = await supabase.auth.signUp(data)
+  if (error) {
+    redirect('/login?message=Error signing up')
+  }
+  revalidatePath('/', 'layout')
+  redirect('/login')
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,8 +8,11 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { emailLogin, signup } from "./actions";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Login({
+export default async function Login({
   searchParams,
 }: {
   searchParams: { message: string };
@@ -19,44 +22,33 @@ export default function Login({
       <Card className="mx-auto max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
+          <CardDescription>Enter your email below to login to your account</CardDescription>
         </CardHeader>
         <CardContent>
           <form id="login-form" className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
+              <Input id="email" name="email" type="email" placeholder="m@example.com" required />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
               </div>
-              <Input
-                minLength={6}
-                name="password"
-                id="password"
-                type="password"
-                required
-              />
+              <Input minLength={6} name="password" id="password" type="password" required />
             </div>
+            {/* 
             {searchParams.message && (
               <div className="text-sm font-medium text-destructive">
                 {searchParams.message}
               </div>
-            )}
-            <Button className="w-full">Login</Button>
+            )} */}
+            <Button formAction={emailLogin} className="w-full">
+              Sign In
+            </Button>
           </form>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <button form="login-form" className="underline">
+            <button formAction={signup} form="login-form" className="underline">
               Sign up
             </button>
           </div>
