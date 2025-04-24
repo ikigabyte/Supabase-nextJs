df26b77d919d84e808248608b8690f5c5f627074
Kigabyte <kevr1999@gmail.com>
2025-04-24 16:06:01 -0400

    Step 7- End of tutorial

--- components/todo-form.tsx
+++ components/todo-form.tsx
@@ -7,19 +7,16 @@ import { Textarea } from "@/components/ui/textarea";
 import { Send } from "lucide-react";
 import { useRef } from "react";
 import { useFormStatus } from "react-dom";
+import { TodoOptimisticUpdate } from "./todo-list";
+import { Todo } from "@/types/custom";
 
 function FormContent() {
   const { pending } = useFormStatus();
   console.log("pending", pending);
   return (
     <>
-      <Textarea
-        minLength={4}
-        name="todo"
-        required
-        placeholder="Add a new todo"
-      />
-      <Button type="submit" size="icon" className="min-w-10" disabled={pending} >
+      <Textarea minLength={4} name="todo" required placeholder="Add a new todo" />
+      <Button type="submit" size="icon" className="min-w-10" disabled={pending}>
         <Send className="h-5 w-5" />
         <span className="sr-only">Submit Todo</span>
       </Button>
@@ -27,7 +24,7 @@ function FormContent() {
   );
 }
 
-export function TodoForm() {
+export function TodoForm({ optimisticUpdate }: { optimisticUpdate: TodoOptimisticUpdate }) {
   const formRef = useRef<HTMLFormElement>(null); // Null by default
   return (
     <Card>
@@ -35,7 +32,15 @@ export function TodoForm() {
         <form
           ref={formRef}
           className="flex gap-4"
-          action={async (data) => {
+          action={async (data) => { // Fake todo for the ui instantly
+            const newTodo : Todo = {
+              id: -1, // useful for optimistic update
+              inserted_at: "",
+              user_id: "",
+              task: data.get("todo") as string,
+              is_complete: false,
+            }
+            optimisticUpdate({action : "create", todo: newTodo})
             await addTodo(data);
             formRef.current?.reset(); // Reset the form after submission
           }}
--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -1,14 +1,44 @@
+'use client';
+
 import { TodoItem } from "./todo-item";
 import { TodoForm } from "./todo-form";
 import { Todo } from "@/types/custom";
+import { useOptimistic } from "react";
+
+export type Action = "delete" | "update" | "create";
+
+export function todoReducer(state: Array<Todo>, { action, todo }: { action: Action; todo: Todo }) {
+  switch (action) {
+    case "delete":
+      return state.filter(({ id }) => id !== todo.id); // deletes the state id
+    case "update":
+      return state.map((t) => (t.id == todo.id ? todo : t)); // change the current todo
+    case "create":
+      return [todo, ...state]; // this is at the start of the array
+    default:
+      return state;
+  }
+}
+
+export type TodoOptimisticUpdate = (action: { action: Action; todo: Todo }) => void;
 
 export function TodoList({ todos }: { todos: Array<Todo> }) {
+  const [optimisticTodos, optimisticTodosUpdate] = useOptimistic(
+    todos,
+    todoReducer
+  );
   return (
     <>
-      <TodoForm />
+      <TodoForm optimisticUpdate={optimisticTodosUpdate} />
       <div className="w-full flex flex-col gap-4">
-        {todos?.map((todo) => {
-          return <TodoItem todo={todo} key={todo.id} />;
+        {optimisticTodos?.map((todo) => {
+          return (
+            <TodoItem
+              optimisticUpdate={optimisticTodosUpdate}
+              todo={todo}
+              key={todo.id}
+            />
+          );
         })}
       </div>
     </>
