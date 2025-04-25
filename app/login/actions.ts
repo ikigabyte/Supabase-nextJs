b4e83a5d6b5800e49f05467bc3b08ff3630089d3
Kigabyte <kevr1999@gmail.com>
2025-04-25 14:47:01 -0400

    debugging for production

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,77 +1,78 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
-import { createClient } from '@/utils/supabase/server'
-import { Provider } from '@supabase/supabase-js'
-import { getURL } from '@/utils/helpers'
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { Provider } from "@supabase/supabase-js";
+import { getURL } from "@/utils/helpers";
 
 // Email and password for formData
-export async function emailLogin(formData: FormData) { 
-  const supabase = await createClient()
+export async function emailLogin(formData: FormData) {
+  const supabase = await createClient();
   // type-casting here for convenience
   // in practice, you should validate your inputs
 
   // todo Form validation methods
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
-  const { error } = await supabase.auth.signInWithPassword(data)
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+  const { error } = await supabase.auth.signInWithPassword(data);
   if (error) {
-    console.error('Error logging in', error)
-    redirect('/login?message=Could not authenticate user')
+    console.error("Error logging in", error);
+    redirect("/login?message=Could not authenticate user");
   }
-  revalidatePath('/', 'layout') // * Clearing the cache for new cache entries with the user logging in
-  console.log("VALID USER LOGGING IN NOW")
+  revalidatePath("/", "layout"); // * Clearing the cache for new cache entries with the user logging in
+  console.log("VALID USER LOGGING IN NOW");
   // console.logdata.
-  redirect('/toprint') // * Redirecting to the todos page
+  redirect("/toprint"); // * Redirecting to the todos page
   // console.log("going in now")
 }
 
 export async function signup(formData: FormData) {
-  const supabase = await createClient() // *Very important from the supabase server file
+  const supabase = await createClient(); // *Very important from the supabase server file
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
-  const { error } = await supabase.auth.signUp(data)
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+  const { error } = await supabase.auth.signUp(data);
   if (error) {
-    redirect('/login?message=Check your email for the confirmation link')
+    redirect("/login?message=Check your email for the confirmation link");
   }
-  revalidatePath('/', 'layout')
-  redirect('/login?message=Check your email for the confirmation link')
+  revalidatePath("/", "layout");
+  redirect("/login?message=Check your email for the confirmation link");
 }
 
-export async function signOut(){
-  const supabase = await createClient() // *Very important from the supabase server file
+export async function signOut() {
+  const supabase = await createClient(); // *Very important from the supabase server file
   await supabase.auth.signOut();
-  console.log("succesfully signed out")
-  redirect('/login');
+  console.log("succesfully signed out");
+  redirect("/login");
 }
 
 export async function oAuthSignIn(provider: Provider) {
   if (!provider) {
     // console.log('No provider selected')
-      return redirect('/login?message=No provider selected')
+    return redirect("/login?message=No provider selected");
   }
 
   // console.log("working here so far")
   const supabase = await createClient();
-  console.log("working here so far")
-  const redirectUrl = getURL("/auth/callback")
+  console.log("working here so far");
+  const redirectUrl = getURL("/auth/callback");
+  console.log("redirectUrl", redirectUrl);
   const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-          redirectTo: redirectUrl,
-      }
-  })
+    provider: "google",
+    options: {
+      redirectTo: redirectUrl,
+    },
+  });
 
   if (error) {
-      redirect('/login?message=Could not authenticate user')
+    redirect("/login?message=Could not authenticate user");
   }
 
-  return redirect(data.url)
+  return redirect(data.url);
 }
