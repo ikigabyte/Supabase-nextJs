9a92541c021f057ae456694cf1bedc5406409029
Kigabyte <kevr1999@gmail.com>
2025-07-29 14:48:24 -0400

    chore: route fix

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,30 +1,30 @@
-import { type EmailOtpType } from '@supabase/supabase-js'
-import { NextResponse, type NextRequest } from 'next/server'
+import { type EmailOtpType } from "@supabase/supabase-js";
+import { NextResponse, type NextRequest } from "next/server";
 
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
+import { getServerClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type') as EmailOtpType | null
-  const next = searchParams.get('next') ?? '/todos' // Default redirect url
+  const { searchParams } = new URL(request.url);
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = searchParams.get("next") ?? "/todos"; // Default redirect url
 
   if (token_hash && type) {
-    const supabase = await createClient()
+    const supabase = await getServerClient();
 
     const { error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
-    })
+    });
     if (!error) {
-      console.log("now going to the next page since we managed to verify the OTP")
+      console.log("now going to the next page since we managed to verify the OTP");
       // redirect user to specified redirect URL or root of app
-      redirect(next)
+      redirect(next);
     }
   }
-  console.log('Error verifying OTP, redirecting to login')
+  console.log("Error verifying OTP, redirecting to login");
 
   // redirect the user to an error page with some instructions
-  redirect('/login?message=Could not veriy OTP')
+  redirect("/login?message=Could not veriy OTP");
 }
