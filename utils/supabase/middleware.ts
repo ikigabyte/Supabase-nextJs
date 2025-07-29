c8dd75d0eac9929ffbd178d885f116716630aaa1
Kigabyte <kevr1999@gmail.com>
2025-07-29 05:53:26 -0400

    test

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -19,7 +19,9 @@ export async function updateSession(request: NextRequest) {
     },
   });
 
+  console.log(request)
   const hasAuth = hasSupabaseAuthCookie(request);
+  console.log("hasAuth", hasAuth);
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
