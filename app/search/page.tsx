db3df42747888acbbe3a5ccecdb0e5c6ae3b0a4d
Kigabyte <kevr1999@gmail.com>
2025-08-05 14:30:11 -0400

    clean: search page fix

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,8 @@
+"use client";
 
-
-import { SearchBar } from "@/components/search-bar";
-import { Order } from "@/types/custom";
-import { Table, TableHead, TableRow, TableHeader } from "@/components/ui/table";
+// import { SearchBar } from "@/components/search-bar";
+// import { Order } from "@/types/custom";
+// import { Table, TableHead, TableRow, TableHeader } from "@/components/ui/table";
 import SearchResults from "@/components/searchresults";
 
 // export default function SearchPage({
@@ -11,15 +11,5 @@ import SearchResults from "@/components/searchresults";
 //   searchParams: { query?: string | string[] };
 // }) {
 export default function SearchPage() {
-
-  // const rawQuery = searchParams.query;
-  // const query = Array.isArray(rawQuery) ? rawQuery[0] : rawQuery ?? '';
-  // console.log(query);
-  return (
-    <div className="p-2 pt-10 max-w-8xl w-[70%] flex flex-col items-center gap-2 relative">
-      {/* <h2 className="font-bold text-lg">Completed</h2> */}
-      {/* // <SearchResults initialQuery={query} /> */}
-      <SearchResults />
-    </div>
-  );
+  return <SearchResults />;
 }
